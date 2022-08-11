eaf9de3268604c36747e6caa1f07fc17b18cc76f
Haja <hajarakotozafy45@gmail.com>
2022-08-11 11:32:13 +0200

    update SQL file

--- SQL/gemi.sql
+++ SQL/gemi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mar. 09 août 2022 à 12:49
+-- Généré le : jeu. 11 août 2022 à 09:30
 -- Version du serveur :  5.7.31
 -- Version de PHP : 7.3.21
 
@@ -20,6 +20,50 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `gemi`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `entretien`
+--
+
+DROP TABLE IF EXISTS `entretien`;
+CREATE TABLE IF NOT EXISTS `entretien` (
+  `num_entretien` int(11) NOT NULL AUTO_INCREMENT,
+  `num_intervenant` int(11) NOT NULL,
+  `num_materiel` int(11) NOT NULL,
+  `date_intervention` datetime NOT NULL,
+  `nb_heures` int(11) NOT NULL,
+  PRIMARY KEY (`num_entretien`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `entretien`
+--
+
+INSERT INTO `entretien` (`num_entretien`, `num_intervenant`, `num_materiel`, `date_intervention`, `nb_heures`) VALUES
+(1, 2, 1, '2022-08-15 14:00:00', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `intervenant`
+--
+
+DROP TABLE IF EXISTS `intervenant`;
+CREATE TABLE IF NOT EXISTS `intervenant` (
+  `num_intervenant` int(11) NOT NULL AUTO_INCREMENT,
+  `nom_intervenant` varchar(255) NOT NULL,
+  `TH_intervenant` int(11) NOT NULL,
+  PRIMARY KEY (`num_intervenant`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `intervenant`
+--
+
+INSERT INTO `intervenant` (`num_intervenant`, `nom_intervenant`, `TH_intervenant`) VALUES
+(2, 'Haja Ny Aina', 60000);
 
 -- --------------------------------------------------------
 
